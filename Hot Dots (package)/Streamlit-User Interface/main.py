59839bcab12135640da7caf994bb7ade4b6b5168
Hao Nguyen <hao.paul.nguyen@gmail.com>
2021-03-09 21:44:24 -0800

    modified streamlit

--- Hot Dots (package)/Streamlit-User Interface/main.py
+++ Hot Dots (package)/Streamlit-User Interface/main.py
@@ -1,3 +1,7 @@
+"""
+This is where the user interface is made using the streamlit package.
+"""
+
 import numpy as np
 import joblib
 import pandas as pd
@@ -5,6 +9,7 @@ import streamlit as st
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
+# Creating questions with multiple choice answer
 RADIO_QUESTIONS_LIST = ['What is your Cadmium source?',
                         'What is your carboxylic acid source?',
                         'What is your amine source?',
@@ -12,21 +17,25 @@ RADIO_QUESTIONS_LIST = ['What is your Cadmium source?',
                         'What is your first solvent?',
                         'What is your second source source?'
                         ]
-RADIO_SELECTIONS = [['cadmium stearate', 'cadmium oxide', 'dimethylcadmium', 'cadmium acetate', 'cadmium acetate dihydrate'],
-                    ['None', 'myrstic acid', 'oleic acid', 'stearic acid', 'benzoic acid', 'dodecylphosphonic acid',
-                     'ethylphosphonic acid', 'lauric acid'],
-                    ['None', '2-6-dimethylpyridine', 'aniline', 'benzylamine', 'dioctylamine/hexadecylamine', 'dodecylamine',
-                     'heptylamine', 'hexadecylamine', 'octadecylamine', 'octylamine', 'oleylamine', 'pyridine', 'trioctylamine'],
+# Creating multiple choices for each question above
+RADIO_SELECTIONS = [['cadmium stearate', 'cadmium oxide', 'dimethylcadmium',
+                            'cadmium acetate', 'cadmium acetate dihydrate'],
+                    ['None', 'myrstic acid', 'oleic acid', 'stearic acid',
+                                'benzoic acid', 'dodecylphosphonic acid',
+                                'ethylphosphonic acid', 'lauric acid'],
+                    ['None', '2-6-dimethylpyridine', 'aniline', 'benzylamine',
+                                'dioctylamine/hexadecylamine', 'dodecylamine',
+                            'heptylamine', 'hexadecylamine', 'octadecylamine',
+                     'octylamine', 'oleylamine', 'pyridine', 'trioctylamine'],
                     ['None', 'diphenylphosphine', 'tributylphosphine',
                         'trioctylphosphine', 'triphenylphosphine'],
                     ['None', 'liquid parafin', 'octadecene',
                         'phenyl ether', 'trioctylphosphine oxide'],
                     ['None', 'phosphinic acid', 'trioctylphosphine oxide']
                     ]
-
-
+# Creating questions wwith slider
 SLIDER_QUESTIONS_LIST = ['How much Cadmium do you plan to use? (mmol)',
-                         'Selenium source is Selenium power, how much Selenium do you plan to use? (mmol)',
+                         'SSelenium power is used; how much Selenium do you plan to use? (mmol)',
                          'How much carboxylic acid  do you plan to use? (mmol)',
                          'How much amine do you plan to use? (mmol)',
                          'How much phosphine do you plan to use? (mmol)',
@@ -35,8 +44,7 @@ SLIDER_QUESTIONS_LIST = ['How much Cadmium do you plan to use? (mmol)',
                          'What is the growth temperature? (Degree Celcius)',
                          'How long do you plan to grow the quantum dots (second)?'
                          ]
-
-
+# Creating sliders for each question above
 SLIDER_SELECTIONS = [[0.1, 14.0, 0.15, 0.0001],
                      [0.001, 1.0, 0.01, 0.0001],
                      [0.0, 60.0, 10.0, 0.001],
@@ -52,10 +60,18 @@ slider_answers = []
 
 
 def get_radio_input(question, selection):
+    """
+    This function creates questions and
+    multiple choice answers in the user interface.
+    """
     answer = st.radio(question, selection)
     return answer
 
 def get_slider_input(question, mmin_val, max_val, default_val, interval):
+    """
+    This function creates questions and
+    slider answers in the user interface.
+    """
     answer = st.slider(question, mmin_val, max_val, default_val, interval)
     return answer
 
@@ -65,29 +81,38 @@ for i in range(len(RADIO_QUESTIONS_LIST)):
 
 for i in range(len(SLIDER_QUESTIONS_LIST)):
     slider_answers.append(get_slider_input(
-        SLIDER_QUESTIONS_LIST[i], SLIDER_SELECTIONS[i][0], SLIDER_SELECTIONS[i][1], SLIDER_SELECTIONS[i][2], SLIDER_SELECTIONS[i][3]))
+        SLIDER_QUESTIONS_LIST[i], SLIDER_SELECTIONS[i][0],
+        SLIDER_SELECTIONS[i][1], SLIDER_SELECTIONS[i][2],
+        SLIDER_SELECTIONS[i][3]))
 
-
-user_input = [slider_answers[7], radio_answers[0], slider_answers[0], slider_answers[1], 
+# Rearange users' choice into a list to input to the ML model
+user_input = [slider_answers[7], radio_answers[0], slider_answers[0], slider_answers[1],
               radio_answers[1], slider_answers[2], radio_answers[2], slider_answers[3],
               radio_answers[3], slider_answers[4], radio_answers[4], slider_answers[5],
               radio_answers[5], slider_answers[6], slider_answers[8]
               ]
-
-user_df = pd.DataFrame(np.array(user_input).reshape(1, -1), columns=['Growth Temp (Celsius)', 'Metal_source', 'Metal_mmol (mmol)',
-                                            'Chalcogen_mmol (mmol)', 'Carboxylic_Acid', 'CA_mmol (mmol)',
-                                            'Amines', 'Amines_mmol (mmol)', 'Phosphines', 'Phosphines_mmol (mmol)',
-                                            'Solvent I', 'S_I_amount (g)', 'Solvent II', 'S_II_amount (g)', 'Time_min (min)'
+# Naming each choice in the user input
+user_df = pd.DataFrame(np.array(user_input).reshape(1, -1), columns=['Growth Temp (Celsius)',
+                                            'Metal_source', 'Metal_mmol (mmol)',
+                                            'Chalcogen_mmol (mmol)', 'Carboxylic_Acid',
+                                            'CA_mmol (mmol)', 'Amines', 'Amines_mmol (mmol)',
+                                            'Phosphines', 'Phosphines_mmol (mmol)',
+                                            'Solvent I', 'S_I_amount (g)',
+                                            'Solvent II', 'S_II_amount (g)', 'Time_min (min)'
                                             ])
 
 st.write(user_df)
 
+# Scaling and encoding user input using the raw dataset
 df = pd.read_csv('CdSe - BetterthanRaw.csv')
 
 #Separate out initial DataFrame into the input features and output features
-df_input = df.drop(columns =['Injection Temp (Celsius)', 'Metal_amount (g)', 'Metal_concentration (mmol/g)', 'Chalcogen_amount (g)',
-'Chalcogen_concentration (mmol/g)', 'Metal/Se_ratio', 'CA_amount (g)', 'Cd/CA_ratio', 'Amines_amount (g)', 'Phosphines_amount (g)', 
-'Chalcogen/Ph_ratio','Total_amount (g)','Chalcogen_source','Diameter_nm', 'Absorbance max (nm)', 'PL max (nm)', 'Diameter from', 'Citation'], inplace = False, axis = 1) 
+df_input = df.drop(columns =['Injection Temp (Celsius)', 'Metal_amount (g)',
+'Metal_concentration (mmol/g)', 'Chalcogen_amount (g)', 'Chalcogen_concentration (mmol/g)',
+'Metal/Se_ratio', 'CA_amount (g)', 'Cd/CA_ratio', 'Amines_amount (g)', 'Phosphines_amount (g)',
+'Chalcogen/Ph_ratio','Total_amount (g)','Chalcogen_source','Diameter_nm', 'Absorbance max (nm)',
+'PL max (nm)', 'Diameter from', 'Citation'],
+inplace = False, axis = 1)
 
 #Checks the column names, and ensures that they do not have any leading or trailing spaces
 df_input.columns = df_input.columns.str.strip()
@@ -107,9 +132,9 @@ ct = ColumnTransformer([
 ct.fit_transform(df_input)
 X = ct.transform(user_df)
 
-
+# Load and use ML model to predict outcomes
 loaded_ET_reg = joblib.load('ET_reg.joblib')
 predicted = loaded_ET_reg.predict(X)
-st.write('Predicted diameter is', predicted[0, 0], '.\n',
-         ' Predicted absorbance max is', predicted[0, 1], '.\n',
-         ' Predicted emission is', predicted[0, 2])
+st.write('Predicted diameter is', predicted[0, 0], '. \n',
+         ' Predicted absorbance max is', predicted[0, 1], '. \n',
+         ' Predicted emission is', predicted[0, 2], '.')
