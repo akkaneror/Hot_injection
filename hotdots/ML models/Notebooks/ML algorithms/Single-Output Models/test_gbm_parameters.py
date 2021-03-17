564a171929bc72a268718b02f1cfdf091f705fdc
Florence Dou <fydou93@gmail.com>
2021-03-17 16:29:07 -0700

    more tests

--- hotdots/ML models/Notebooks/ML algorithms/Single-Output Models/test_gbm_parameters.py
+++ hotdots/ML models/Notebooks/ML algorithms/Single-Output Models/test_gbm_parameters.py
@@ -1,6 +1,9 @@
 """Nosetests for gbm_parameter functions"""
 
+import numpy as np
 import pandas as pd
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from gbm_parameters import highest_r2_calculator
 
@@ -18,8 +21,36 @@ x_train, x_test, y_train, y_test = train_test_split(
 					x, y, test_size=0.25, random_state=8, shuffle=True)
 
 def test_1():
-    """not sure yet"""
+    """make sure best estimator is actually in defined range"""
     best_estimator=highest_r2_calculator(x_train, y_train, x_test, y_test)
     low=1
     high=10
     assert low<best_estimator<high, "estimator outside of range"
+
+def test_2():
+    """make sure r2 is between -1 and 1"""
+    best_estimator=highest_r2_calculator(x_train, y_train, x_test, y_test)
+    gbm = GradientBoostingRegressor(n_estimators=best_estimator)
+    gbm.fit(x_train, y_train)
+    y_predict = gbm.predict(x_test)
+    r2_value = r2_score(y_test, y_predict)
+    assert -1 < r2_value < 1, "r2 outside expected range"
+
+def test_3():
+    """make sure we are actually pulling out highest r2"""
+    best_estimator=highest_r2_calculator(x_train, y_train, x_test, y_test)
+    r2_dict = {}
+    low = 1
+    high = 10
+    for estimator in np.arange(low, high, 1):
+        gbm = GradientBoostingRegressor(n_estimators=estimator)
+        gbm.fit(x_train,y_train)
+        y_predict = gbm.predict(x_test)
+        r2_value = r2_score(y_test, y_predict)
+        r2_dict[estimator] = r2_value
+    return r2_dict
+    gbm = GradientBoostingRegressor(n_estimators=best_estimator)
+    gbm.fit(x_train, y_train)
+    y_predict = gbm.predict(x_test)
+    r2_value = r2_score(y_test, y_predict)
+    assert r2_value==max(r2_dict), "max r2 and calculated r2 do not match"
